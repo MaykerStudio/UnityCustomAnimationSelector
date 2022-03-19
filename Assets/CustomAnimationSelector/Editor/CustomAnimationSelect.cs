45b2af666d4423b2b7cff603c4561a66e2e92569
Mayke Rodrigues (Mike) <maykefrankley@gmail.com>
2022-03-19 12:11:30 -0300

    Version 1.0

---
+++ Assets/CustomAnimationSelector/Editor/CustomAnimationSelect.cs
@@ -0,0 +1,126 @@
+#if UNITY_2019_1_OR_NEWER
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Animations;
+using UnityEditor.IMGUI.Controls;
+using UnityEngine;
+
+public class CustomAnimationSelect : EditorWindow
+{
+    public static CustomAnimationSelect Instance
+    {
+        get
+        {
+            if (!_instance)
+            {
+                ShowWindow();
+            }
+
+            return _instance;
+        }
+    }
+
+    private static CustomAnimationSelect _instance;
+
+    private static GameObject selected;
+
+    private bool isLocked;
+
+    [MenuItem("Tools/Custom animation select")]
+    public static void ShowWindow()
+    {
+        _instance = GetWindow<CustomAnimationSelect>(utility: true, title: "Search clip");
+        _instance.minSize = new Vector2(100, 50);
+        _instance.maxSize = new Vector2(100, 50);
+    }
+
+    private void OnGUI()
+    {
+        if(!isLocked)
+            selected = Selection.activeGameObject;
+
+        if (GUILayout.Button("Clips"))
+        {
+            if (selected)
+            {
+                var animator = selected.GetComponent<Animator>();
+                AnimatorController controller = null;
+
+                if (animator)
+                    controller = AssetDatabase.LoadAssetAtPath<AnimatorController>(AssetDatabase.GetAssetPath(animator.runtimeAnimatorController));
+                else
+                    Debug.LogError($"{selected.name} doesn't contains an animator controller");
+
+                if (controller)
+                {
+                    AdvancedGenericMenu menu = new AdvancedGenericMenu();
+
+                    for (int i = 0; i < controller.animationClips.Length; i++)
+                    {
+                        AnimationClip c = controller.animationClips[i];
+                        menu.AddItem(controller.animationClips[i].name, false, () => 
+                        {
+                            var animWindow = GetWindow<AnimationWindow>();
+                            animWindow.Focus();
+                            animWindow.animationClip = c;
+                        });
+                    }
+
+                    Rect rect = new Rect();
+
+                    menu.DropDown(rect);
+
+                    float maxHeight = 300f;
+
+                    var window = focusedWindow;
+
+                    if (window == null)
+                    {
+                        Debug.LogWarning("EditorWindow.focusedWindow was null.");
+                        return;
+                    }
+
+                    if (!string.Equals(window.GetType().Namespace, typeof(AdvancedDropdown).Namespace))
+                    {
+                        Debug.LogWarning("EditorWindow.focusedWindow " + focusedWindow.GetType().FullName + " was not in expected namespace.");
+                        return;
+                    }
+
+                    var position = window.position;
+                    if (position.height <= maxHeight)
+                    {
+                        return;
+                    }
+
+                    position.height = maxHeight;
+                    window.minSize = position.size;
+                    window.maxSize = position.size;
+                    window.position = position;
+                    window.ShowAsDropDown(GUIUtility.GUIToScreenRect(rect), position.size);
+                }
+            }
+        }
+
+        if (!isLocked)
+        {
+            if (selected)
+            {
+                if (GUILayout.Button(new GUIContent("Lock", "Lock the current selected object")))
+                    isLocked = true;
+            }
+        }
+        else
+        {
+            GUI.contentColor = Color.red;
+            if (GUILayout.Button(new GUIContent("Unlock", "Unlock the current selected object")))
+            {
+                isLocked = false;
+            }
+        }
+
+        if (Application.isPlaying)
+            isLocked = false;
+    }
+}
+#endif
